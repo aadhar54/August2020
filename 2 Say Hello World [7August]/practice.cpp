3878a571901b4711ae020ce50ec4202fd590ea1a
react ninja <aadhar54@gmail.com>
2020-08-12 21:13:56 +0530

    Files added for 12 August Session

---
+++ 2 Say Hello World [7August]/practice.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	//cout<<"Hello World by August Batch Mentees , who are awesome"<<endl;
+	// int num ;
+	// cout<<num<<endl;
+	// char ch ;
+	// cout<<ch<<endl;
+	// ch = 'a';
+	// num = 10;
+	// cout<<ch<<endl;
+	// cout<<num<<endl;
+	// string zuck = "I made Facebook";
+	// cout<<zuck<<endl;
+	int num1,num2,sum;
+	cout<<"Enter num1\n";
+	cin>>num1;
+	cout<<"Enter num2\n";
+	cin>>num2;
+	sum = num1+num2;
+	cout<<"The sum is "<<sum<<"\n";
+
+
+//This prints some string .ie .Hello World by August Batch Mentees , who are awesome .
+	return 0;
+}
+
+/*
+
+Hello 
+I am 
+Aadhar 
+I 
+love to code.
+*/
