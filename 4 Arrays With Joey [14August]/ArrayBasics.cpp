58dcfbc0ef551dc755f75df5c6188b1a1b86198e
react ninja <aadhar54@gmail.com>
2020-08-19 21:11:09 +0530

    Files added for 19 August session Pointers

--- 4 Arrays With Joey [14August]/ArrayBasics.cpp
+++ 4 Arrays With Joey [14August]/ArrayBasics.cpp
@@ -3,18 +3,26 @@ using namespace std;
 
 int main(){
 
-	int a[100];
+	int a[100]; // defining array with 100 elements
 
 	//0 ........99 
 	a[0]=5;
-	cout<<a[1]<<endl; 
+	cout<<a[1]<<endl; // 0 
 
 
 	int b[5];
 	for(int j=0;j<5;j++){
+		// 0<5
+		// 1<5
+		// 2<5
+		// 3<5
+		// 4<5
+		//loop runs five times
 		cout<<"Enter "<<j<<" element"<<endl;
 		cin>>b[j];  // User Input 
 		cout<<b[j]<<endl;
+
+
 	}
 
 	return 0;
